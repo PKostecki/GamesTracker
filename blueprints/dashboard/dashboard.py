fd225e9e17d1f40236c62a8a70767d91bd97eeae
PKostecki <zreth99@gmail.com>
2022-03-03 23:51:10 +0100

    Implemented recently finished segment

--- blueprints/dashboard/dashboard.py
+++ blueprints/dashboard/dashboard.py
@@ -1,8 +1,10 @@
 from flask import render_template, Blueprint, request, redirect, url_for, session
 from log_required import login_required
-from flask_login import logout_user
+from database import DatabaseExecutes
+import os
 
 dashboard_blueprint = Blueprint("dashboard", __name__, template_folder='templates')
+database_executor = DatabaseExecutes(os.path.join("gametracker_database.db"))
 
 
 @dashboard_blueprint.route('', methods=['POST', 'GET'])
@@ -21,9 +23,31 @@ def dashboard():
             session.pop('name', None)
             return redirect(url_for('login.login'))
     else:
-        return render_template('dashboard.html'), 201
+        record_info = get_records()
+        last_records_list = []
+        for game, grade, user_id in record_info:
+            game_name = get_game_name(game)
+            user = get_user(user_id)
+            last_record_tuple = (game_name, grade, *user)
+            last_records_list.append(last_record_tuple)
+        return render_template('dashboard.html', last_records_list=last_records_list[-5:]), 201
 
 
+def get_records():
+    record_info = database_executor.get_records_query("""SELECT game_id, record_grade,  user_id FROM  
+    records;""")
+    return record_info
 
 
+def get_game_name(game_id):
+    game_name = database_executor.select_single_element(f"""SELECT game_name FROM games WHERE
+     game_id == '{game_id}';""")
+    if game_name is not None:
+        game_name = game_name[0]
+    return game_name
+
+
+def get_user(user_id):
+    user = database_executor.get_select_list(f"""SELECT user_nickname FROM users WHERE user_id = '{user_id}';""")
+    return user
 
